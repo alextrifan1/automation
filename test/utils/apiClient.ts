158a2a43520f1e3bff4beeb86c9d6e2c1e464f56
alextrifan1 <alexandrutrifan17@gmail.com>
2025-10-22 14:54:57 +0300

    feat: implemented api client and some tests

---
+++ test/utils/apiClient.ts
@@ -0,0 +1,19 @@
+import axios, { AxiosResponse  } from "axios";
+
+const BASE_URL = process.env.BASE_API_URL || 'https://fakestoreapi.com';
+
+export const apiClient = {
+    async get(endpoint: string): Promise<AxiosResponse> {
+        return axios.get(`${BASE_URL}${endpoint}`);
+    },
+
+    async post(endpoint: string, body: object): Promise<AxiosResponse> {
+        return axios.post(`${BASE_URL}${endpoint}`, body);
+    },
+    async put(endpoint: string, body: object): Promise<AxiosResponse> {
+        return axios.put(`${BASE_URL}${endpoint}`, body);
+    },
+    async delete(endpoint: string): Promise<AxiosResponse> {
+        return axios.delete(`${BASE_URL}${endpoint}`);
+    },
+};
--- wdio.conf.ts
+++ wdio.conf.ts
@@ -3,15 +3,26 @@ import type { Options } from '@wdio/types';
 import { openApp } from './test/utils/navigation.ts';
 import { browser } from '@wdio/globals';
 
-export const config = {
+const isApiTest = process.env.TEST_TYPE === 'api';
+
+export const config: Options.Testrunner = {
   runner: 'local',
   tsConfigPath: './tsconfig.json',
+  
+  specs: process.env.SPEC_TO_RUN
+      ? process.env.SPEC_TO_RUN.split(',')
+      : isApiTest
+          ? ['./test/specs/api/**/*.ts']
+          : ['./test/specs/ui/**/*.ts'],
 
-  specs: process.env.SPEC_TO_RUN ? process.env.SPEC_TO_RUN.split(',') : ['./test/specs/**/*.ts'],
-
+  //
+  // Capabilities: only if UI tests
+  //
   maxInstances: 10,
+  capabilities: isApiTest
+      ? [{ browserName: 'chrome', maxInstances: 1, 'wdio:headless': true }]  // dummy capability for API tests
+      : [{ browserName: 'chrome' }],
 
-  capabilities: [{ browserName: 'chrome' }],
 
   logLevel: 'info',
   bail: 0,
@@ -23,14 +34,19 @@ export const config = {
   reporters: ['spec'],
   mochaOpts: { ui: 'bdd', timeout: 60000 },
 
-  before: async function (capabilities, specs) {
-    await openApp(process.env.START_PATH ?? '/');
+  //
+  // Hooks
+  //
+  before: async function () {
+    if (!isApiTest) {
+      await openApp(process.env.START_PATH ?? '/');
+    }
   },
 
-  afterTest: async function (test, context, { error, result, duration, passed, retries }) {
-    if (!passed) {
+  afterTest: async function (test, context, { passed }) {
+    if (!isApiTest && !passed) {
       const timestamp = new Date().toISOString().replace(/:/g, '-');
       await browser.saveScreenshot(`./errorShots/${test.title}_${timestamp}.png`);
     }
   },
-} as unknown as Options.Testrunner;
+};
